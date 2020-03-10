98ef5b3e71af7e71258e5fa4194fa407e8b31efe
seungjae yoon <tmdwo5811@gmail.com>
2020-03-10 16:05:32 +0900

    express static 파일 불러오기 실습

--- expressTest/app.js
+++ expressTest/app.js
@@ -1,11 +1,16 @@
 var express = require('express');
 var app = express();
+app.use(express.static('public'));
+
 app.get('/', (req, res) => { //app.get = '라우터' 라는 역할, 라우팅 = 길을 찾다.
-    res.send('Hello home page');
+    res.send('Hello home           page');
 });
 app.get('/login',(req, res) => {
-    res.send('Login please');
-})
+    res.send('Login adsfasdfasdfplease');
+});
+app.get('/index',(req, res) => {
+    res.send('HTML');
+});
 app.listen(3000, () => {
     console.log('Connected 3000 port!');
 });
