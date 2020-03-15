8a5bee1c236eb69d2df618e75849f3eb7ee116cd
seungjae yoon <tmdwo5811@gmail.com>
2020-03-15 13:53:36 +0900

    ES6 async 와 await 활용 코드가 간결해 진다.

--- expressTest/db-find.js
+++ expressTest/db-find.js
@@ -1,15 +1,18 @@
 // //ES6
 
-// const {MongoClient, ObjectID} = require('mongodb');
+const {MongoClient, ObjectID} = require('mongodb');
 
-// const uri = 'mongodb://localhost:27017';
+const uri = 'mongodb://localhost:27017';
 
-// (async function (){
-//     try{
-//         const client = await MongoClient.connect(uri, { useNewUrlParser: true })
+(async function (){
+    try{
+        const client = await MongoClient.connect(uri, { useNewUrlParser: true })
         
-//         const db = client.db('Zsls')
-//     } catch(e){
-//         console.log(e);
-//     }
-// })()
+        const db = client.db('Zsls');
+        db.collection('Human').find({_id: 811045678451}).toArray().then((docs) => {
+            console.log((JSON.stringify(docs, undefined,2)));
+        });
+    } catch(e){
+        console.log(e,"오류입니다.");
+    }
+})()
