3e2ee5db90b351d938857d05cbaccc2f82b7db71
seungjae yoon <tmdwo5811@gmail.com>
2020-03-15 14:07:08 +0900

    Vjs convert to ES6 DB Connect by Sant_J

---
+++ expressTest/db-connectEs6.js
@@ -0,0 +1,22 @@
+const {MongoClient, ObjectID} = require('mongodb');
+
+const uri = 'mongodb://localhost:27017';
+
+const connectDb = async () => {
+    try{
+        const client = await MongoClient.connect(uri,{ useNewUrlParser: true})
+        const testData = {
+            name: 'ConnectByEs6',
+            age: 28,
+            nationality: 'Korean'
+        };
+        const db = client.db('Zsls');
+        db.collection('Human').insertOne(testData, () => {
+            console.log('몽고 DB 입력 성공 =>', testData)
+        });
+    }catch(e){
+        console.log(e,'에러가 발생하였습니다.');
+    }
+    client.close();
+}
+connectDb();
