5abd0bc9120b5f99e1cd3336ca8f7ee6132801c2
seungjae yoon <tmdwo5811@gmail.com>
2020-03-21 15:20:40 +0900

    Scope 테스트

--- niCoJsPractice/test.js
+++ niCoJsPractice/test.js
@@ -1,4 +1,18 @@
-const cal = (a, b) => {
-    return a**b;
+var variable = 'functionScope';
+
+function functionScope () {
+    var variable2 = 'functionScope2';
+    console.log(variable, '함수 실행'); // 출력
+    console.log(variable2, '함수 실행 2')
 }
-console.log(cal(3,10));
+console.log(variable); //출력
+//console.log(variable2); //에러
+functionScope();
+
+let obj = {
+    testString : 1,
+    testFunc : () => {
+        console.log(testString)}
+}
+
+console.log(obj.testString, '오브젝트 Key값 호출');
