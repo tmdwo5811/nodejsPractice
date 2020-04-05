c2e192a4a5282d9cf9df858e6b34e40922a471fb
seungjae yoon <tmdwo5811@gmail.com>
2020-04-05 15:06:15 +0900

    todo List 작성

--- expressTest/app.js
+++ expressTest/app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 app.use(express.static('public'));
-
+//도커 설치 해야함.
   
 app.get('/', (req, res) => { //app.get = '라우터' 라는 역할, 라우팅 = 길을 찾다.
     res.send('');
